29f7035d27d6633b41ada4bb011a44493e274ca4
hdc.hggforget <hdc.hggforget@bytedance.com>
2024-03-31 12:51:21 +0800

    WIP: Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-cd NL2SQL_partial_matching/
+# 获取当前脚本的绝对路径
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+# 获取当前脚本的目录路径
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+echo "The script is located at: $SCRIPT_PATH"
+
+
 pip3 install poetry
 poetry install
 
 # 获取 Poetry 管理的虚拟环境的路径
 VENV_PATH=$(poetry env info -p)
-
 # 激活虚拟环境
 source "$VENV_PATH/bin/activate"
 
-pip3 install /opt/tiger/NL2SQL_partial_matching/GMN/dependencies/torch_scatter-2.0.9-cp39-cp39-linux_x86_64.whl
-pip3 install /opt/tiger/NL2SQL_partial_matching/GMN/dependencies/torch_sparse-0.6.17+pt113cu117-cp39-cp39-linux_x86_64.whl
+# 定义相对路径
+TORCH_SCATTER_RELATIVE_PATH="/GMN/dependencies/torch_scatter-2.0.9-cp39-cp39-linux_x86_64.whl"
+TORCH_SPARSE_RELATIVE_PATH="/GMN/dependencies/torch_sparse-0.6.17+pt113cu117-cp39-cp39-linux_x86_64.whl"
 
-
-# 运行你的 Python 脚本
-# python3 GMN/train.py
+pip3 install "${SCRIPT_DIR}${TORCH_SCATTER_RELATIVE_PATH}"
+pip3 install "${SCRIPT_DIR}${TORCH_SPARSE_RELATIVE_PATH}"
